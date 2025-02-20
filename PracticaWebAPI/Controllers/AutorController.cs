5ef4678271cee6988c43eca3961ad0f0d19768e9
CarcasXD <carlossalesianoprro@gmail.com>
2025-02-20 00:31:39 -0600

    Autor Realizado

--- PracticaWebAPI/Controllers/AutorController.cs
+++ PracticaWebAPI/Controllers/AutorController.cs
@@ -26,6 +26,30 @@ namespace PracticaWebAPI.Controllers
             }
             return Ok(listadoAutor);
         }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id)
+        {
+            var autor = (from e in _bibliotecaContext.Autor
+                            join t in _bibliotecaContext.Libro
+                            on e.id_autor equals t.id_autor
+                            where e.id_autor == id
+                            select new
+                            {
+                                e.id_autor,
+                                e.nombre,
+                                e.nacionalidad,
+                                t.titulo
+                            }).ToList();
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(autor);
+        }
         [HttpPost]
         [Route("CrearAutor")]
         public IActionResult GuardarBiblioteca([FromBody] Autor autor)
@@ -40,6 +64,43 @@ namespace PracticaWebAPI.Controllers
             {
                 return BadRequest(ex.Message);
             }
+        }
+
+        [HttpPut]
+        [Route("modificar/{id}")]
+        public IActionResult actualizarAutor(int id, [FromBody] Autor autorActualizar)
+        {
+            Autor? autorActual = (from e in _bibliotecaContext.Autor where e.id_autor == id select e).FirstOrDefault();
+
+            if (autorActual == null)
+            {
+                return NotFound();
+            }
+
+            autorActual.nombre = autorActualizar.nombre;
+            autorActual.nacionalidad = autorActualizar.nacionalidad;
+
+            _bibliotecaContext.Entry(autorActual).State = EntityState.Modified;
+            _bibliotecaContext.SaveChanges();
+
+            return Ok(autorActual);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult eliminarAutor(int id)
+        {
+            Autor? autor = (from e in _bibliotecaContext.Autor where e.id_autor == id select e).FirstOrDefault();
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            _bibliotecaContext.Autor.Attach(autor);
+            _bibliotecaContext.Autor.Remove(autor);
+            _bibliotecaContext.SaveChanges(true);
+            return Ok(autor);
         }
     }
 
