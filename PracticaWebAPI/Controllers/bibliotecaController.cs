41fb47a8856f47cc2cc1b8b60a19f1c865fb2eb0
CarcasXD <carlossalesianoprro@gmail.com>
2025-02-19 21:20:54 -0600

    Biblioteca controlador

---
+++ PracticaWebAPI/Controllers/bibliotecaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PracticaWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PracticaWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class bibliotecaController : ControllerBase
+    {
+        private readonly bibliotecaContext _bibliotecaContext;
+
+        public bibliotecaController(bibliotecaContext bibliotecaContext) 
+        {
+            _bibliotecaContext = bibliotecaContext;
+        }
+    }
+}
