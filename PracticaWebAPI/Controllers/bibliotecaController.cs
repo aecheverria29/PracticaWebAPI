99e329f9b141b7bdb37a434e194a0552fa34a2a1
alex <ee653522@gmail.com>
2025-02-19 21:55:21 -0600

    Nuevo controlller

--- PracticaWebAPI/Controllers/bibliotecaController.cs
+++ PracticaWebAPI/Controllers/bibliotecaController.cs
@@ -1,19 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using PracticaWebAPI.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using PracticaWebAPI.Models;
 
 namespace PracticaWebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class bibliotecaController : ControllerBase
+    public class BibliotecaController : ControllerBase
     {
         private readonly bibliotecaContext _bibliotecaContext;
 
-        public bibliotecaController(bibliotecaContext bibliotecaContext) 
+        public BibliotecaController(bibliotecaContext bibliotecaContext)
         {
             _bibliotecaContext = bibliotecaContext;
+        }
+
+        [HttpGet] // ← Añadir este método para que Swagger lo detecte
+        public IActionResult Get()
+        {
+            return Ok("Biblioteca API funcionando correctamente.");
         }
     }
 }
